5ab345c25ddede809a14a343790caf74ad7e6d78
wangxinyu <wangweimo1998@163.com>
2022-05-09 17:51:29 +0800

    初始化项目

---
+++ xiaotuxian-pc-client（2版）/js/index.js
@@ -0,0 +1,65 @@
+// 页面滚动，显示与隐藏电梯导航
+(function () {
+	const elevator = document.querySelector('.xtx-elevator');
+	const backTop = document.querySelector('#backTop');
+	const entry = document.querySelector('.xtx_entry');
+	
+	window.addEventListener('scroll', function () {
+		const ddes = document.documentElement.scrollTop;
+		// if (ddes >= 300) {
+		//   elevator.style.opacity = 1;
+		// } else {
+		//   elevator.style.opacity = 0;
+		// }
+		elevator.style.opacity = ddes >= entry.offsetTop ? 1 : 0;
+	});
+// 点击返回顶部
+	backTop.addEventListener('click', function () {
+		// document.documentElement.scrollTop = 0;
+		window.scrollTo(0, 0);
+	});
+})();
+
+(function () {
+	const list = document.querySelector('.xtx-elevator-list');
+	// 点击电梯导航，滑动到对应位置
+	list.addEventListener('click', function (e) {
+		if (e.target.tagName === 'A' && e.target.dataset.name) {
+			// 排他 ，移除active
+			const old = document.querySelector('.xtx-elevator-list .active');
+			// 判断，active不存在则不移除（防止报错）
+			if (old) {
+				old.classList.remove('active');
+			}
+			// 当前元素添加active
+			e.target.classList.add('active');
+			// 获取对应模块的offsetTop
+			const top = document.querySelector(`.xtx_goods_${e.target.dataset.name}`).offsetTop;
+			// 页面滚动到对应位置
+			document.documentElement.scrollTop = top;
+		}
+	});
+	
+	// 页面滚动到对应位置，电梯导航选中指定模块
+	window.addEventListener('scroll', function () {
+		// 移除active
+		const old = document.querySelector('.xtx-elevator-list .active');
+		if (old) old.classList.remove('active');
+		
+		// 判断页面当前位置，选中导航模块
+		const news = document.querySelector('.xtx_goods_new');
+		const popular = document.querySelector('.xtx_goods_popular');
+		const brand = document.querySelector('.xtx_goods_brand');
+		const topic = document.querySelector('.xtx_goods_topic');
+		const ds = document.documentElement.scrollTop;
+		if (ds >= news.offsetTop && ds < popular.offsetTop) {
+			document.querySelector('[data-name=new]').classList.add('active');
+		} else if (ds >= popular.offsetTop && ds < brand.offsetTop) {
+			document.querySelector('[data-name=popular]').classList.add('active');
+		} else if (ds >= brand.offsetTop && ds < topic.offsetTop) {
+			document.querySelector('[data-name=brand]').classList.add('active');
+		} else if (ds >= topic.offsetTop) {
+			document.querySelector('[data-name=topic]').classList.add('active');
+		}
+	});
+})();
